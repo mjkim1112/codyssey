2428057d0d8f7c1a4405cfcdfd3542369aa0ec93
mjkim1112 <mjkim6931@gmail.com>
2025-07-14 19:26:33 +0900

    Add files via upload

---
+++ 과정1/문제3/calculator.py
@@ -0,0 +1,43 @@
+def add(a,b):
+        return a+b
+
+def subtract(a,b):
+        return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    if b==0:
+        print("Error:Division by zero")
+        return None
+    else:
+        return a/b
+
+if __name__=="__main__":
+
+    try:
+        a,b = map(float, input("실수형 숫자 2개 입력").split())
+    except:
+        print("Invalid Input")
+        exit()
+    
+    a=int(a)
+    b=int(b)
+    
+    op=input("연산자 입력")
+    
+    if op not in ('+', '-', '*', '/'):
+        print("Invalid operator")
+    if op=='+':
+        c=add(a,b)
+        print("Result:", c)
+    elif op=="-":
+        c=subtract(a,b)
+        print("Result:", c)
+    elif op=="*":
+        c=multiply(a,b)
+        print("Result:", c)
+    elif op=="/":
+        c=divide(a,b)
+        print("Result:", c)
