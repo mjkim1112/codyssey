e99b5b191a1a28913cba8bc17decb76b22646206
mjkim1112 <mjkim6931@gmail.com>
2025-07-16 15:46:54 +0900

    Add files via upload

--- 과정1/문제4/minmax_calculator.py
+++ 과정1/문제4/minmax_calculator.py
@@ -1,1 +1,23 @@
+def main():
+    try:
+        seq = list(map(float, input("숫자 입력: ").split()))
+    except ValueError:
+        print("Invalid Input")
+        exit()
+    if not seq:
+        print("none")
+        return
+        
+    min = seq[0]
+    max = seq[0]
+    
+    for num in seq[1:]:
+        if num < min:
+            min= num
+        if num > max:
+            max = num
 
+    print("Min:", min, "Max:", max)
+
+if __name__ == "__main__":
+    main()
